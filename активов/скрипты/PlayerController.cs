9a8e3f85fc46b296fe8ce865030dc436cecc7c5d
PatientNobody <33672765+PatientNobody@users.noreply.github.com>
2017-12-19 20:42:03 +0300

    Text
    Добавление подсчета очков и добавление текста о выигрыше.

--- активов/скрипты/PlayerController.cs
+++ активов/скрипты/PlayerController.cs
@@ -7,9 +7,25 @@ public class PlayerController : MonoBehaviour {
     public float speed = 10f;
     private Rigidbody rb;
 
+    public int countCube;
+    public int countCapsule;
+    public int countCylinder;
+
+    public Text countTextCube;
+    public Text countTextCapsule;
+    public Text countTextCylinder;
+    public Text winText;
+
     void Start ()
     {
         rb = GetComponent<Rigidbody>();
+        countCube = 0;
+        countCapsule = 0;
+        countCylinder = 0;
+        winText.text = "";
+        setCountCube();
+        setCountCapsule();
+        setCountCylinder();
     }
 
     void FixedUpdate()
@@ -23,7 +39,45 @@ public class PlayerController : MonoBehaviour {
     }
 
 	void OnTriggerEnter (Collider other) {
-		if ((other.tag == "Cube")||(other.tag == "Capsule")||(other.tag == "Cylinder"))
-			Destroy (other.gameObject);
+		if (other.tag == "Cube")
+        {
+            Destroy(other.gameObject);
+            countCube = countCube + 5;
+            setCountCube();
+        }
+        if (other.tag == "Capsule")
+        {
+            Destroy(other.gameObject);
+            countCapsule = countCapsule + 10;
+            setCountCapsule();
+        }
+        if (other.tag == "Cylinder")
+        {
+            Destroy(other.gameObject);
+            countCylinder = countCylinder + 15;
+            setCountCylinder();
+        }
 	}
+
+    void setCountCube ()
+    {
+        countTextCube.text = "Cubes: " + countCube.ToString ();
+        if ((countCube == 20) && (countCapsule == 30) && (countCylinder == 75))
+            winText.text = "You Win!";
+    }
+
+    void setCountCapsule ()
+    {
+        countTextCapsule.text = "Capsules: " + countCapsule.ToString();
+        if ((countCube == 20) && (countCapsule == 30) && (countCylinder == 75))
+            winText.text = "You Win!";
+    }
+
+    void setCountCylinder ()
+    {
+        countTextCylinder.text = "Cylinders: " + countCylinder.ToString();
+        if ((countCube == 20) && (countCapsule == 30) && (countCylinder == 75))
+            winText.text = "You Win!";
+    }
+
 }
